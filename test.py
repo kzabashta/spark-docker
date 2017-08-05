550271f85f0f493b3ea535983458870eb39b020e
kzabashta <kosta.zabashta@gmail.com>
2017-08-05 00:54:29 -0400

    Make the test application actually print something...

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import pyspark
 conf = pyspark.SparkConf()
 
-conf.setMaster("spark://192.168.99.100:7077")
+conf.setMaster("spark://<docker machine IP>:7077")
 conf.setAppName('test')
 
 sc = pyspark.SparkContext(conf=conf)
 
 rdd = sc.parallelize(range(100))
+print(rdd.reduce(lambda x,y: x+y))
